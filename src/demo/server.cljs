fe50327aa488299f75d0fc0f494424a6172d3943
Michael SALIHI <admin@prestance-informatique.fr>
2021-07-15 17:23:11 +0200

    Move project in subdirectory

--- src/demo/server.cljs
+++ src/demo/server.cljs
@@ -1,8 +1,10 @@
-(ns server
+(ns demo.server
   (:require ["express" :as express]
-            [reagent.dom.server :as d]))
+            [reagent.dom.server :as d]
+            [demo.core :as core]
+            ["path" :as path]))
 
-(defn template []
+(defn template [body]
   [:html
    [:head
     [:meta {:charset "utf-8"}]
@@ -10,10 +12,11 @@
             :content "width=device-width, initial-scale=1.0"}]]
    [:body
     [:div#app
-     [:h1 "Server Rendering!!!"]]]])
+     [body]]
+    [:script {:src "/js/app.js"}]]])
 
 (defn render-page [path]
-  (d/render-to-static-markup [template]))
+  (d/render-to-static-markup [template core/app-view]))
 
 (defn handle-request [req res]
   (.send res (render-page (.-path req))))
@@ -21,5 +24,6 @@
 (defn main []
   (let [port 3000]
     (-> (express)
+        (.use (.static express (.resolve path "public")))
         (.get "/" handle-request)
         (.listen port #(println "Server started on port" port)))))
